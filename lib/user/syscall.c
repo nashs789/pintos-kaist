bec2fc68a72df514fa37de15cadd363abcf30933
poqop721 <poqop721@naver.com>
2023-10-06 16:44:33 +0900

    16_44

--- lib/user/syscall.c
+++ lib/user/syscall.c
@@ -1,6 +1,7 @@
 #include <syscall.h>
 #include <stdint.h>
 #include "../syscall-nr.h"
+#include "../../include/threads/init.h"
 
 __attribute__((always_inline))
 static __inline int64_t syscall (uint64_t num_, uint64_t a1_, uint64_t a2_,
@@ -71,6 +72,7 @@ static __inline int64_t syscall (uint64_t num_, uint64_t a1_, uint64_t a2_,
 void
 halt (void) {
 	syscall0 (SYS_HALT);
+	// power_off();
 	NOT_REACHED ();
 }
 
