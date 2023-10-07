eaa2977053d6fe0216721c81246a18c33bff9e44
nashs789 <bbock1214@gmail.com>
2023-10-07 11:13:00 +0000

    do.sh

--- do.sh
+++ do.sh
@@ -2,6 +2,7 @@ cd userprog
 cd build
 make clean
 make
-pintos --fs-disk=10 -p tests/userprog/exit:exit -- q -f run 'exit'
-pintos --fs-disk=10 -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
+# pintos --fs-disk=10 -p tests/userprog/exit:exit -- -q -f run 'exit'
+pintos --fs-disk=10 -p tests/userprog/create-empty:create-empty -- -q -f run 'create-empty'
+# pintos --fs-disk=10 -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
 # pintos --gdb --fs-disk=10 -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
